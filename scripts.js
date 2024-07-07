a8e377fe90a8f88b626258bf97a23849e8a82245
Santabo <75633586+Santabo@users.noreply.github.com>
2024-07-07 14:12:54 +0100

    Improved overall look of website

--- scripts.js
+++ scripts.js
@@ -26,7 +26,7 @@ function loadThemes() {
 function createThemeElement(themeData) {
     const themeElement = document.createElement('div');
     themeElement.classList.add('theme');
-    
+
     const creatorsList = themeData.createdBy.map(creatorId => {
         const creator = window.creators.find(c => c.id === creatorId);
         return creator ? `<img src="${creator.avatarUrl}" alt="${creator.displayName}" title="${creator.displayName}">` : '';
@@ -34,9 +34,11 @@ function createThemeElement(themeData) {
 
     themeElement.innerHTML = `
         <img src="${themeData.image}" alt="${themeData.name}">
-        <h2>${themeData.name}</h2>
-        <div class="download-links">
-            <a href="${themeData.downloadLinks[0].url}" target="_blank" class="download-link">${themeData.downloadLinks[0].label}</a>
+        <div class="theme-info">
+            <h2>${themeData.name}</h2>
+            <div class="download-links">
+                <a href="${themeData.downloadLinks[0].url}" target="_blank" class="download-link">${themeData.downloadLinks[0].label}</a>
+            </div>
         </div>
         <div class="creator-avatars">${creatorsList}</div>
     `;
